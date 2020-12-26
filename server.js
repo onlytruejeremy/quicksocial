797cb423ae8b9100845babc0da4dc3725af14b55
onlytruejeremy <jeremydev2020@gmail.com>
2020-12-25 17:01:06 -0800

    Heroku Changes

--- server.js
+++ server.js
@@ -17,3 +17,15 @@ app.listen(port, () => {
 });
 
 app.use("/api/users", require("./routes/user"));
+
+//serve static if production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on: ${port}`);
+});
